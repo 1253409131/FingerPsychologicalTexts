7527b68283a2167ca32e67b840e54610f900e1d6
QinJunzhen <1253409131@qq.com>
2016-03-06 19:31:00 +0800

    2016年3月6号所有代码

---
+++ FingerPsychologicalTexts/FingerPsychologicalTexts/New/Views/StartTextTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  StartTextTableViewCell.m
+//  FingerPsychologicalTexts
+//
+//  Created by scjy on 16/3/6.
+//  Copyright © 2016年 秦俊珍. All rights reserved.
+//
+
+#import "StartTextTableViewCell.h"
+
+@implementation StartTextTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ FingerPsychologicalTexts/FingerPsychologicalTexts/New/Views/StartTextView.m
@@ -0,0 +1,21 @@
+//
+//  StartTextView.m
+//  FingerPsychologicalTexts
+//
+//  Created by scjy on 16/3/6.
+//  Copyright © 2016年 秦俊珍. All rights reserved.
+//
+
+#import "StartTextView.h"
+
+@implementation StartTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
